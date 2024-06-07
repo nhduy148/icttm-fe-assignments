1aa6c2e5b711745fcf90ed0c80d3716fd978593b
Duy Nguyen <33407104+nhduy148@users.noreply.github.com>
2024-06-08 02:07:59 +0700

    chore: trigger build

--- vite.config.ts
+++ vite.config.ts
@@ -6,10 +6,10 @@ import svgr from 'vite-plugin-svgr';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), svgr()],
+  base: '/demo',
   resolve: {
     alias: {
       '@': resolve(__dirname, './src'),
     },
   },
-  base: '/demo',
 });
